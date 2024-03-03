80695f8997831e8046739573ee742eef4fae929a
LJJbyZJU <111830843+LJJbyZJU@users.noreply.github.com>
2024-03-03 04:46:01 +0000

    百度访问统计接入 Updated By Github Actions

---
+++ js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?84465dae335b92304cbe8ed7d51f1e43";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
