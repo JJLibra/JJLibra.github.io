2fbd8b9bff46e23aea1d4186c318f2bdcd262f42
JJLibra <111830843+JJLibra@users.noreply.github.com>
2024-12-08 09:34:09 +0000

    🎈 perf: gulp tensor Updated By Github Actions

--- js/baidutongji.js
+++ js/baidutongji.js
@@ -1,7 +1,1 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?84465dae335b92304cbe8ed7d51f1e43";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
+var _hmt=_hmt||[];!function(){var e=document.createElement("script");e.src="https://hm.baidu.com/hm.js?84465dae335b92304cbe8ed7d51f1e43";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
